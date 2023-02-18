0e5c695bf5ea704b40b494b05feee6f83c4cbddc
Richard <git.nordstrom@gmail.com>
2023-02-18 20:22:35 +0100

    add cmdline cmp dependency

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,12 +44,13 @@ return packer.startup(function(use)
 	use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
 	use({ "hrsh7th/cmp-buffer" }) -- buffer completions
 	use({ "hrsh7th/cmp-path" }) -- path completions
+	use({ "hrsh7th/cmp-cmdline" }) -- cmdline completions
 	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
 	use({ "hrsh7th/cmp-nvim-lsp" })
 	use({ "hrsh7th/cmp-nvim-lua" })
 
 	-- Snippets
-	use({ "L3MON4D3/LuaSnip" }) --snippet engine
+	use({ "L3MON4D3/LuaSnip", run = "make install_jsregexp" }) --snippet engine
 	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
 	-- LSP
