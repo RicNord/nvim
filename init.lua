bfe3b05143101be2d299856dea2acb5138e18152
Richard <git.nordstrom@gmail.com>
2023-02-19 11:27:34 +0100

    forced to run in vscode, dont load plugins

--- init.lua
+++ init.lua
@@ -1,11 +1,18 @@
-require("user.options")
-require("user.keymaps")
-require("user.plugins")
-require("user.colorscheme")
-require("user.cmp")
-require("user.lsp")
-require("user.telescope")
-require("user.gitsigns")
-require("user.treesitter")
-require("user.autopairs")
-require("user.undotree")
+-- Check if running vscode extension or not
+-- https://marketplace.visualstudio.com/items?itemName=asvetliakov.vscode-neovim
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+    require("user.options")
+    require("user.keymaps")
+    require("user.plugins")
+    require("user.colorscheme")
+    require("user.cmp")
+    require("user.lsp")
+    require("user.telescope")
+    require("user.gitsigns")
+    require("user.treesitter")
+    require("user.autopairs")
+    require("user.undotree")
+end
