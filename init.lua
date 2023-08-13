bcfaa7bdd66d2d1a67bc58d697c456d1d1b04e06
RicNord <109558073+RicNord@users.noreply.github.com>
2023-08-13 20:38:04 +0200

    Update init.lua with WSL clipboard

--- init.lua
+++ init.lua
@@ -16,3 +16,20 @@ else
     require("user.autopairs")
     require("user.undotree")
 end
+
+-- See `:h clipboard-wsl`
+local in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+if in_wsl then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ["+"] = { 'clip.exe' },
+            ["*"] = { 'clip.exe' },
+        },
+        paste = {
+            ["+"] = { 'wsl-paste' },
+            ["*"] = { 'wsl-paste' },
+        },
+        cache_enabled = 0,
+    }
+end
