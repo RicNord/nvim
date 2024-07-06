c55133dc2129490ff3094841561d56d6d7d9915d
Richard Nordström <git.nordstrom@gmail.com>
2024-07-06 23:06:16 +0200

    dont use loop for opt assignments

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,43 +1,37 @@
-local options = {
-	swapfile = false,
-	backup = false, -- creates a backup file
-	undodir = os.getenv("HOME") .. "/.vim/undodir",
-	undofile = true,
+vim.opt.swapfile = false
+vim.opt.backup = false -- creates a backup file
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-	cmdheight = 2, -- more space in the neovim command line for displaying messages
-	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-	conceallevel = 0, -- so that `` is visible in markdown files
-	ignorecase = true, -- ignore case in search patterns
-	mouse = "a", -- allow the mouse to be used in neovim
-	showmode = true,
-	showtabline = 2, -- always show tabs
+vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
+vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+vim.opt.showmode = true
+vim.opt.showtabline = 2 -- always show tabs
 
-	smartcase = true, -- smart case
-	autoindent = true, -- apply current row indentation on new created adjacent row
-	smartindent = true, -- make indenting smarter with help of syntax/style
+vim.opt.smartcase = true -- smart case
+vim.opt.autoindent = true -- apply current row indentation on new created adjacent row
+vim.opt.smartindent = true -- make indenting smarter with help of syntax/style
 
-	splitbelow = true, -- force all horizontal splits to go below current window
-	splitright = true, -- force all vertical splits to go to the right of current window
-	termguicolors = true, -- set term gui colors (most terminals support this)
-	updatetime = 300, -- faster completion (4000ms default)
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
+vim.opt.updatetime = 300 -- faster completion (4000ms default)
 
-	expandtab = true, -- convert tabs to spaces
-	shiftwidth = 4, -- the number of spaces inserted for each indentation
-	tabstop = 4, -- show 4 spaces for a tab (some git providers use 8 by default)
-	softtabstop = 4, -- insermode tab option
-	number = true, -- set numbered lines
-	relativenumber = true, -- set relative numbered lines
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- show 4 spaces for a tab (some git providers use 8 by default)
+vim.opt.softtabstop = 4 -- insermode tab option
+vim.opt.number = true -- set numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
 
-	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-	wrap = false, -- display lines as one long line
-	linebreak = true, -- companion to wrap, don't split words
-	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
-	sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
-	colorcolumn = "80",
-}
-
-for k, v in pairs(options) do
-	vim.opt[k] = v
-end
+vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+vim.opt.wrap = false -- display lines as one long line
+vim.opt.linebreak = true -- companion to wrap, don't split words
+vim.opt.scrolloff = 8 -- minimal number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8 -- minimal number of screen columns either side of cursor if wrap is `false`
+vim.opt.colorcolumn = "80"
 
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- separate vim plugins from neovim in case vim still in use
