04dc22d5894ccdfa3586ae3963a601612a0a4971
Richard Nordström <git.nordstrom@gmail.com>
2024-11-14 00:45:46 +0100

    pyright and ruff coexist better

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -4,11 +4,5 @@ return {
 			-- Using Ruff's import organizer
 			disableOrganizeImports = true,
 		},
-		python = {
-			analysis = {
-				-- Ignore all files for analysis to exclusively use Ruff for linting
-				ignore = { "*" },
-			},
-		},
 	},
 }
