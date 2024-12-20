8648fd71eafb596f256dd7e5f48901b6fc5e6da9
Richard Nordström <git.nordstrom@gmail.com>
2024-12-20 16:42:11 +0100

    fix cmp plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,5 @@
 return {
     "hrsh7th/nvim-cmp",
-    version = false,
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lua",
@@ -14,10 +13,7 @@ return {
             build = "make install_jsregexp",
             config = function()
                 local ls = require("luasnip")
-
-                -- Load VS Code-style snippets after LuaSnip is loaded
-                local vs_ls = require("luasnip/loaders/from_vscode")
-                vs_ls.lazy_load() -- Snippets
+                require("luasnip/loaders/from_vscode").lazy_load() -- Load VS Code-style snippets
 
                 -- Key mappings for jumping through snippets
                 vim.keymap.set({ "i", "s" }, "<C-L>", function()
@@ -27,34 +23,12 @@ return {
                 vim.keymap.set({ "i", "s" }, "<C-J>", function()
                     ls.jump(-1) -- Jump back
                 end, { silent = true })
-            end
-        },
-    },
-    opts = {
-        snippet = {
-            expand = function(args)
-                require("luasnip").lsp_expand(args.body)
             end,
         },
     },
-    config = function()
+    opts = function()
         local cmp = require("cmp")
-        cmp.mapping.preset.insert({
-            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.abort(),
-            ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        })
-        cmp.config.sources({
-            { name = "nvim_lua" },
-            { name = "nvim_lsp" },
-            { name = "luasnip" },
-            { name = "path" },
-        }, {
-            { name = "buffer", keyword_length = 3 },
-        })
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        -- Setup for `/` and `?`
         cmp.setup.cmdline({ "/", "?" }, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
@@ -62,7 +36,7 @@ return {
             },
         })
 
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        -- Setup for `:`
         cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
@@ -70,7 +44,28 @@ return {
             }, {
                 { name = "cmdline" },
             }),
-            matching = { disallow_symbol_nonprefix_matching = false },
         })
-    end
+        return {
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lua" },
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "path" },
+            }, {
+                { name = "buffer", keyword_length = 3 },
+            }),
+        }
+    end,
 }
