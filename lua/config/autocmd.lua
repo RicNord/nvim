6751c26686f2673c07082b1f1b6372dc0032fbdf
Richard Nordström <git.nordstrom@gmail.com>
2024-12-20 16:42:51 +0100

    autocmd fixes

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,20 +1,14 @@
--- Use internal formatting for bindings like gq.
---vim.api.nvim_create_autocmd("LspAttach", {
---    callback = function(args)
---        vim.bo[args.buf].formatexpr = nil
---    end,
---})
-
--- Set colorcolumn specifics for filetypes
---local group = vim.api.nvim_create_augroup("colorcolumn_options", { clear = true })
---vim.api.nvim_create_autocmd({ "FileType", "BufEnter", "BufAdd" }, {
---	group = group,
---	callback = function(opts)
---		if vim.bo[opts.buf].filetype == "python" then
---			vim.opt["colorcolumn"] = "88" -- Black default
---		else
---			vim.opt["colorcolumn"] = "80"
---		end
---	end,
---    buffer = 0
---})
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+            return
+        end
+        if client.name == 'ruff' then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+        end
+    end,
+    desc = 'LSP: Disable hover capability from Ruff',
+})
